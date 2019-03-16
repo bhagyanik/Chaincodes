8c9534fad40c85efe8587b8fb076a4980069174b
bhagyanik <46862303+bhagyanik@users.noreply.github.com>
2019-03-16 15:11:33 +0530

    Update BR31A1.go

--- BR31A1.go
+++ BR31A1.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
-    "bytes"
+    //"bytes"
 	"encoding/json"
-    "strconv"
+    //"strconv"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	 pb "github.com/hyperledger/fabric/protos/peer"
 	)
 
 // Define the chaincode structure
-type Chaincode1 struct {
+type SimpleChaincode struct {
 }
 
 //Define DWPB structure 
 type DWPB struct {
-	UniqueID     		string    `json:"UniqueID"`
-	Latest_approved_value	float64   `json:"Latest_approved_value"`
-	Latest_utilised_value	float64   `json:"Latest_utilised_value"`
-	Latest_remaining_value	float64   `json:"Latest_remaining_value"`
-	ModifiedDate 		int64	  `json:"Modified_Date"` // timestamp
-	Version			string    `json:"Version"`
-	Expired    		bool	  `json:"Expired"`
-	Budget_sub_status	string    `json:"Budget_sub_status"`
+	UniqueID     			string    `json:"UniqueID"`
+	Latest_approved_value	string  `json:"Latest_approved_value"`
+	Latest_utilised_value	string   `json:"Latest_utilised_value"`
+//1	Latest_remaining_value	float64   `json:"Latest_remaining_value"`
+//1	ModifiedDate 			int64	  `json:"Modified_Date"` // timestamp
+//1	Version					string    `json:"Version"`
+//1	Expired      			bool	  `json:"Expired"`
+//1	Budget_sub_status		string    `json:"Budget_sub_status"`
 }
 
-func (s *Chaincode1) Init (APIstub shim.ChaincodeStubInterface, args[] string) pb.Response {
-	fmt.Println ("Chaincode1 init")
+func (s *SimpleChaincode) Init (APIstub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
 }
 
-func (s *Chaincode1) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response {
+func (s *SimpleChaincode) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response {
 
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
@@ -42,38 +42,37 @@ func (s *Chaincode1) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response {
 }    
 
 
-func (s *Chaincode1) createDWPB (APIstub shim.ChaincodeStubInterface) pb.Response {
-    if len(args) != 8 {
+func (s *SimpleChaincode) createDWPB (APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
+    if len(args) != 3 {
 		return shim.Error("Incorrect number of arguments. Expecting 8")
 	}
 
 // populate DWPB
-	Dwpb := DWPB {
-	UniqueID  = args[0]
-	Latest_approved_value = args[1]
-	Latest_utilised_value = args[2]
-	Latest_remaining_value = args[3]
-	ModifiedDate = args[4]
-	Version = args[5]
-	Expired  = args[6]
-	Budget_sub_status = args[7]
-	}	
+	var Dwpb = DWPB{UniqueID: args[0],Latest_approved_value: args[1],Latest_utilised_value: args[2]}
+//	Latest_utilised_value = args[2]
+//	Latest_remaining_value = args[3]
+//	ModifiedDate = args[4]
+//	Version = args[5]
+//	Expired  = args[6]
+//	Budget_sub_status = args[7]
+//	}
 //store state in ledger
 	DWPBAsBytes,_ := json.Marshal(Dwpb)	
-	err := APIstub.PutState(UniqueID,DWPBAsBytes)
+	err := APIstub.PutState(args[0],DWPBAsBytes)
 	if err != nil {
 	return shim.Error(fmt.Sprintf("Failed to create DWPB"))
 	}
+	return shim.Success(nil)
 }
 //function to update the existing DWPB
 
-//func (s *Chaincode1) UpdateDWPB (APIstub shim.ChaincodeStubInterface) pb.Response {
+//func (s *SimpleChaincode) UpdateDWPB (APIstub shim.ChaincodeStubInterface) pb.Response {
 //}
 // to be written
 //update for one unique ID 	
 
 //function to query a DWPB
-func (s *Chaincode1) queryDWPB (APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (s *SimpleChaincode) queryDWPB (APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
   if len(args) != 1 {
     return shim.Error("Incorrect number of arguments. Expecting UniqueID")
   }
@@ -84,4 +83,11 @@ func (s *Chaincode1) queryDWPB (APIstub shim.ChaincodeStubInterface, args []stri
   return shim.Success(DWPBAsBytes)
 }
 
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+	
 
